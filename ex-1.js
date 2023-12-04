03267d48a78fa163c881109bf7da4f469073e383
Dennis <suwatchai.kaew@gmail.com>
2023-12-04 10:33:29 +0700

    feat: add ex1-2 v1

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,17 @@
 
 function forEach(array, operation) {
   // Start coding here
+  operation(array);
+}
+function add5000(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    newEmployeeSalaries.push(employeeSalaries[i] + 5000);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
-
+forEach(employeeSalaries, add5000);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,17 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let scoreMoreThan70 = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      scoreMoreThan70 += 1;
+    }
+  }
+  console.log(scoreMoreThan70);
+  return operation(scoreMoreThan70);
+}
+function isAtLeastFive(amount) {
+  return amount >= 5 ? true : false;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -13,3 +24,9 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+scoreRoom1Result = atLeastFive(studentScoresRoom1, isAtLeastFive);
+scoreRoom2Result = atLeastFive(studentScoresRoom2, isAtLeastFive);
+scoreRoom3Result = atLeastFive(studentScoresRoom3, isAtLeastFive);
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
